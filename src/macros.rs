23de782f860a5e6e5ae076cc25eeb3e9c13b1485
italic <super3dblender@yahoo.com>
2020-05-14 19:08:19 -0700

    Macros: Add back clone macro (renamed `mclone!()`) for non-UI cloning.
    UI's clone!() requires glib, which I want to keep entirely in its own module. This clone is simpler and does not pull in a dependency.

---
+++ src/macros.rs
@@ -0,0 +1,18 @@
+/// Borrowed from the gtk-rs docs and tutorial
+#[macro_export]
+macro_rules! mclone {
+    (@param _) => ( _ );
+    (@param $x:ident) => ( $x );
+    ($($n:ident),+ => move || $body:expr) => (
+        {
+            $( let $n = $n.clone(); )+
+            move || $body
+        }
+    );
+    ($($n:ident),+ => move |$($p:tt),+| $body:expr) => (
+        {
+            $( let $n = $n.clone(); )+
+            move |$(mclone!(@param $p),)+| $body
+        }
+    );
+}
